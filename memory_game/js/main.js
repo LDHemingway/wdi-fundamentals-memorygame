005442b51e0f5455f7b9169c3c8bac0e1e854a2c
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-08-13 16:52:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,14 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('queen');
+console.log("User flipped queen");
+var cardTwo = cards[3];
+cardsInPlay.push('king');
+console.log("User flipped king");
+if (length.cardsInPlay === 2);
+	if (cards[0] === cards[3]) {
+		alert("You found a match!")}
+	else {
+	 	alert("Sorry, try again.")
+	 }
